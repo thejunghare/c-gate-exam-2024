423cc6386170a428767e82f56fa1084ee0fe1052
thejunghare <prasadjunghare2@gmail.com>
2023-10-24 17:21:37 +0530

    add more comment

--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -1,7 +1,12 @@
+// this is the boiler plate of C
+
 #include <stdio.h> // with out this header file compiler will don't know what is printf
 
+// main function
 int main(void) {
+  // void -> return type of main function
   printf("Hello World!\n");
-  //  f in the printf stands for formatted
+  //  f in the printf stands for formatted, it acts as an placeholder and substitute for variable (more on this in next folders) it let the compiler know what type the variable is when value of output is unkown
+
   // \n is an espace character used to print a new line
 }
