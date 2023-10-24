c6973ff6f76e2c79e562e893794f39aaa2a4d95c
thejunghare <prasadjunghare2@gmail.com>
2023-10-24 17:20:49 +0530

    add some comment

--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // with out this header file compiler will don't know what is printf
 
-int main(void){
-  printf("Hello World!");
+int main(void) {
+  printf("Hello World!\n");
+  //  f in the printf stands for formatted
+  // \n is an espace character used to print a new line
 }
