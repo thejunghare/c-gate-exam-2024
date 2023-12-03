619d751c0ec1740ec3d124db9eaa80a5b5cc9da4
thejunghare <prasadjunghare2@gmail.com>
2023-12-03 20:13:19 +0530

    fin types

--- 02-types/main.c
+++ 02-types/main.c
@@ -20,9 +20,52 @@ uint64_t
 'int' can't represent value with floating point
 for eg. int mynumber = 3; // correct
 & int mynumber = 3.4 // incorrect
+
+This is where floating Number comes
+
+'CHAR'
+char => often use to store character and number, stores numberical value of the
+char in the variable.
+
+
+'ARRAYS'
 */
 
+struct person {
+  char name[50];
+  int age;
+  int height_in_inches;
+};
+
 int main(void) {
-  printf("Intro to types in c");
+  printf("Intro to types in c \n");
+
+  // INT
+  int mynumber = 9;
+  printf("%i\n", mynumber);
+
+  // CHAR
+  char c = 45;
+  printf("%c\n", c);
+
+  // ARRAY
+  int x[50];
+  x[0] = 5;
+  x[3] = 500;
+  printf("%i, %i\n", x[0], x[3]);
+
+  // STRUCT
+  struct person me;
+  struct person you;
+
+  me.age = 22;
+  you.height_in_inches = 4;
+  printf("%i, %i\n", me.age, you.height_in_inches);
+
+  // POINTER
+  int y = 7;
+  int *p = &y; // x here is not int and just an address
+  printf("%p\n", p);
+
   return 0;
 }
