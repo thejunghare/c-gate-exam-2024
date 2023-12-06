f8aca5b38ae5841865d540c08775f779879a2d33
thejunghare <prasadjunghare2@gmail.com>
2023-12-06 12:18:29 +0530

    add comments

---
+++ 05-comments/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* Comments are very important. Comments are things which you write in your program which aren't code[machine is gonna ignore them] */
+
+    // single line comment
+
+    /* Multi line comment */
+
+    // Maintain your commments [update description]
+    return 0;
+}
